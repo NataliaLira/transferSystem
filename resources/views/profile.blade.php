72b10f81b40b5f2260aab4f96fa0fd9b76d41dc9
Natalia Lira <natalia.rlira@gmail.com>
2020-08-25 09:51:43 -0300

    chore: blocking sent transfers from cnpj users

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -10,6 +10,28 @@
 <h1>Bem vindo(a) {{$name}}</h1>
 @if(auth()->user()->type == 1)
 <a href="/transfer"><button>Realizar Transferência</button></a> 
+
+<div>
+    <h1>Transferências Realizidas</h1>
+    <table>
+        <thead>
+            <tr>
+                <th scope="col">Quando</th>
+                <th scope="col">Valor</th>
+                <th scope="col">Para</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($sent as $transfer)
+            <tr>
+                <td>{{$transfer->created_at}}</td>
+                <td>{{$transfer->value}}</td>
+                <td>{{$transfer->name}}</td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
 @endif
 <div>
     <h1>Transferências Recebidas</h1>
@@ -23,27 +45,6 @@
         </thead>
         <tbody>
             @foreach ($received as $transfer)
-            <tr>
-                <td>{{$transfer->created_at}}</td>
-                <td>{{$transfer->value}}</td>
-                <td>{{$transfer->name}}</td>
-            </tr>
-            @endforeach
-        </tbody>
-    </table>
-</div>
-<div>
-    <h1>Transferências Realizidas</h1>
-    <table>
-        <thead>
-            <tr>
-                <th scope="col">Quando</th>
-                <th scope="col">Valor</th>
-                <th scope="col">Para</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($sent as $transfer)
             <tr>
                 <td>{{$transfer->created_at}}</td>
                 <td>{{$transfer->value}}</td>
