4a232bdff060dd88b589fffab6db11cef5b2f716
Natalia Lira <natalia.rlira@gmail.com>
2020-08-25 06:58:41 -0300

    feature: authentication and users migrations

--- database/migrations/2020_10_12_000000_create_users_table.php
+++ database/migrations/2020_10_12_000000_create_users_table.php
@@ -16,9 +16,11 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
+            $table->bigInteger('doc_id')->unique();
+            $table->string('email');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->foreignId('type_id')->constrained('types');
             $table->rememberToken();
             $table->timestamps();
         });
---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>TransferMoney</title>
+</head>
+<body>
+    <h1>Transfer Money</h1>
+    <h3>A segurança que seu dinheiro precisa!</h3>
+    <form method="POST" action="{{ route('register') }}">
+        @csrf
+
+        <div class="form-group row">
+            <label for="name" class="col-md-4 col-form-label text-md-right">Nome Completo</label>
+
+            <div class="col-md-6">
+                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+
+                @error('name')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
+            </div>
+        </div>
+
+        <div class="form-group row">
+            <label for="name" class="col-md-4 col-form-label text-md-right">Tipo De Cadastro</label>
+
+            <select name="type" id="">
+                <option value="" selected disabled>escolha uma opção</option>
+                <option value="1">Pessoa Física</option>
+                <option value="2">Pessoa Jurídica</option>
+            </select>
+        </div>
+
+        <div class="form-group row">
+            <label for="cpf" class="col-md-4 col-form-label text-md-right">CPF/CNPJ</label>
+
+            <div class="col-md-6">
+                <input id="cpf" type="text" class="form-control @error('doc_id') is-invalid @enderror" name="doc_id" value="{{ old('doc_id') }}" required autocomplete="doc_id" autofocus>
+
+                @error('cpf')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
+            </div>
+        </div>
+
+        <div class="form-group row">
+            <label for="email" class="col-md-4 col-form-label text-md-right">E-mail:</label>
+
+            <div class="col-md-6">
+                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+
+                @error('email')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
+            </div>
+        </div>
+
+        <div class="form-group row">
+            <label for="password" class="col-md-4 col-form-label text-md-right">Senha</label>
+
+            <div class="col-md-6">
+                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+
+                @error('password')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
+            </div>
+        </div>
+
+        <div class="form-group row">
+            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">Confirme Sua Senha</label>
+
+            <div class="col-md-6">
+                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+            </div>
+        </div>
+
+        <div class="form-group row mb-0">
+            <div class="col-md-6 offset-md-4">
+                <button type="submit" class="btn btn-primary">
+                    Cadastre-se
+                </button>
+            </div>
+        </div>
+    </form>
+</body>
+</html>
